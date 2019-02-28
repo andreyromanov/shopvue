74bc3dc6c4a175b72974de585382ee9b930e6e22
andreyromanov <andreygorogogo@gmail.com>
2019-02-28 21:46:35 +0200

    Navbar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import 'bootstrap';
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+import 'popper.js';
+import './assets/app.scss'
 
 Vue.config.productionTip = false;
+
+Vue.component('Navbar', require('./components/Navbar.vue').default);
 
 new Vue({
   router,
