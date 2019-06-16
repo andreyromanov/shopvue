34f8906d4172e3edeb8c6f0dc1be0bac10b4ec26
andreyromanov <andreygorogogo@gmail.com>
2019-06-16 23:41:14 +0300

    Add product to cart

--- src/components/AddToCart.vue
+++ src/components/AddToCart.vue
@@ -1,44 +1,55 @@
 <template>
   <div class="add-to-cart">
-    
- <button class="btn btn-primary float-left">Add to cart</button>
-    
+
+    <button class="btn btn-primary float-left" @click="addToCart">Add to cart</button>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: "add-to-cart",
-  props: {
-    name: String,
-    price: String,
-    productID: String
-  },
+  export default {
+    name: "add-to-cart",
+    props: {
+      name: String,
+      price: String,
+      productID: String
+    },
 
-    data(){
-        return{
-        productName: this.name,
-        productPrice: this.price,
-        product_id: this.id  
+    data() {
+      return {
+        item: {
+          productName: this.name,
+          productPrice: this.price,
+          product_id: this.id
         }
+      }
+    },
+    methods: {
+      addToCart() {
+        $('#miniCart').modal('show');
+        this.$store.commit('addToCart', this.item);
+      }
     }
-};
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
